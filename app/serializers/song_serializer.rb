387a6ca863a276e8c0ec0d46d034c00108b6d8f2
Lucas Siqueira <lucas.asiq@gmail.com>
2016-06-05 22:02:03 -0300

    fixed songs

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -2,9 +2,5 @@ class SongSerializer < ActiveModel::Serializer
   attributes :id, :name, :download_url
   has_one :album
   has_one :genre
-
-  def download_url
-      object.download_url
-  end
   
 end
