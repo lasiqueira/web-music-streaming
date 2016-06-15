6d573837c117c063c413ccf560742f93e44b1e38
Lucas Siqueira <lucas.asiq@gmail.com>
2016-06-15 19:07:01 -0300

    removed unused methods from MusicPlayer component

--- src/js/pages/MusicPlayer.js
+++ src/js/pages/MusicPlayer.js
@@ -8,23 +8,6 @@ import SessionStore from "../stores/SessionStore";
 
 export default class MusicPlayer extends React.Component {
   
-  constructor() {
-    super();
-    this._onChange = this._onChange.bind(this);
-  }
-  
-  componentDidMount() {
-    SessionStore.addChangeListener(this._onChange);
-  }
-
-  componentWillUnmount() {
-    SessionStore.removeChangeListener(this._onChange);
-  }
-
-  _onChange() {
-      //this.setState(getStateFromStores());
-  }
-
   render() {
     return (
       <div class="container-fluid">
