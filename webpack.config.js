a13f6f6d180dbea005cc758c6f466019c64d9f2e
Lucas Siqueira <lucas.asiq@gmail.com>
2016-06-19 14:26:18 -0300

    removed babel-polyfill from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var path = require('path');
 module.exports = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : null,
-  entry: ["babel-polyfill","./js/client.js"],
+  entry: ["./js/client.js"],
   module: {
     
     loaders: [
